0add88390f2529e88e4a4cfe185cbdec1d21c791
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-03-28 18:15:33 +0100

    add faq tests

---
+++ src/test/java/praktikum/FaqTests.java
@@ -0,0 +1,45 @@
+package praktikum;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.pages.FaqPage;
+
+@RunWith(Parameterized.class)
+public class FaqTests {
+    private final String itemId;
+
+    @ClassRule
+    public static DriverRule driverRule = new DriverRule();
+
+    public FaqTests(String itemId) {
+        this.itemId = itemId;
+    }
+
+    @BeforeClass
+    public static void closeCookies() {
+        new FaqPage(driverRule.getDriver())
+                .open()
+                .acceptCookies();
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] faqData() {
+        return new Object[][]{
+                {"0"},
+                {"1"},
+                {"2"},
+                {"3"}
+        };
+    }
+
+    @Test
+    public void clickOnFaqItem() {
+        new FaqPage(driverRule.getDriver())
+                .checkAnswerIsInvisible(itemId)
+                .clickOnQuestion(itemId)
+                .waitForAnswer(itemId);
+    }
+}
