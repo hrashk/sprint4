07b4e19520b28fa52e2c78ff62b29192e4132af5
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2025-01-20 18:01:47 +0100

    add driver ule

--- src/test/java/praktikum/SampleWebTest.java
+++ src/test/java/praktikum/SampleWebTest.java
@@ -1,30 +1,37 @@
 package praktikum;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
 
 public class SampleWebTest {
-    private WebDriver driver;
-
-    @Before
-    public void startUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-    }
+    @Rule
+    public DriverRule factory = new DriverRule();
 
     @Test
     public void openMainPage() throws Exception {
+        WebDriver driver = factory.getDriver();
+
         driver.get("https://qa-scooter.praktikum-services.ru/");
 
-        Thread.sleep(10_000);
-    }
+        driver.findElement(By.className("Header_Link__1TAG7")).click();
 
-    @After
-    public void tearDown() {
-        driver.quit();
+        driver.findElement(By.xpath(".//input[contains(@class,'Input_Input__1iN_Z')]")).sendKeys("123");
+
+        driver.findElement(By.cssSelector(".Header_Button__28dPO")).click();
+
+        By notFoundImage = By.cssSelector("img[alt='Not found']");
+
+        new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.visibilityOfElementLocated(notFoundImage));
+
+        assertTrue(driver.findElement(notFoundImage).isDisplayed());
     }
 }
